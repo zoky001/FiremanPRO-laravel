fc0b5dd4e49abead7025eb71431e22bde8da5fe4
Zoran Hrnčić <zorhrncic@foi.hr>
2018-02-24 11:52:09 +0100

    FORCE SCHEME -> https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,16 +4,18 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
+
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
         //
+        if (env('APP_ENV') == 'production' || 1) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
@@ -21,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         //
     }
+
 }
